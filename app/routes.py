3f583323b08df268e5bc3917405ba50c420b044f
Kim Hart <kimhart90@gmail.com>
2016-01-19 15:55:13 -0500

    initial styling and routes

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return render_template('layout.html')
+@app.route('/<name>')
+def hello_world(name=None):
+  return render_template('layout.html', name=name)
 
 @app.route('/home')
 def home():
   return render_template('layout.html')
-
-@app.route('/home/')
-@app.route('/home/<name>')
-def home(name=None):
-  return render_template('home.html', name=name)
 
 @app.route('/flask')
 def flask():
